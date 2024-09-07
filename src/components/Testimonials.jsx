30143d0fdf696e0c4f8bc16b79f3fe055cf2944d
Bharath Kumar Reddy Vemireddy <vemireddybharath@lpu.in>
2024-09-07 22:37:12 +0530

    adding caed deal  section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import {feedback} from "../constants";
+import styles from "../style";
+import FeedbackCard from "./FeedbackCard";
 
-const Testimonials = () => {
-  return (
+
+
+const Testimonials = () => (
     <div>Testimonials</div>
   )
-}
+
 
 export default Testimonials
